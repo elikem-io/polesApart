854f0104ef91f2b8e474c2957f1626ff2e3923ae
elikem-io <ielikemamevor@gmail.com>
2019-07-02 18:11:11 -0700

    javascript workaround, basically connection to the uinames API

--- app.js
+++ app.js
@@ -1,27 +1,64 @@
 document.getElementById('submit').addEventListener('click', loadUser);
 
-//variables
-const xhr = new XMLHttpRequest(),
-      link = "https://randomuser.me/api/",
-      //key = "MDB3-YF7H-IWED-8LS6";
-      gender ="gender=" + document.querySelector ('#gender').value,
-      race = document.querySelector ('#race').value,
-      age = "age=" + document.querySelector ('#myRange').value;
-      url = link + age;
-
-
-
-//functions
 function loadUser(e){
     e.preventDefault();
+
+        const gender = document.querySelector ('#gender').value,
+                country = document.querySelector ('#country').value,
+                age = document.querySelector ('#age').value,
+                amount = document.querySelector('#quantity').value;
+
+        link = "https://uinames.com/api/?";
+
+        if(country !== ''){
+            link += `region=${country}&`;
+        }
+        if(gender !== ''){
+            link += `gender=${gender}&`;
+        }
+        if(amount !== ''){
+            link += `amount=${amount}&`;
+        }
+        
     //open a new connection
-    console.log(age);
     
+    const xhr = new XMLHttpRequest();
     xhr.open('GET', link, true);
     xhr.onload = function(){
         if(this.status === 200){
             const user = JSON.parse(this.responseText);
-            let 
+            document.querySelector('#user-profile').innerHTML =
+            `
+            <div class="profile">
+            <div class="img">
+                <img src="x.jpg" alt="">
+            </div>
+            <div class="details">
+                <div class="name">
+                    <h1>Name:</h1> <p>${user.name}</p>
+                </div>
+                <div class="age">
+                    <h1>Age:</h1> <p>${user.age}</p>
+                </div>
+                <div class="interests">
+                    <h1>Interests:</h1> <p>Ball</p>
+                </div>
+                <div class="profession">
+                    <h1>Profession:</h1> <p>Ball</p>
+                </div>
+                <div class="need">
+                <h1>Needs / wants:</h1> <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa aperiam ullam quibusdam.</p>
+                </div>
+               <div class="goals">
+                    <h1>Goals / Aspirations</h1> <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa aperiam ullam quibusdam.</p>
+                </div>
+                <div class="buttons">
+                    <button>Edit</button>
+                    <button>Download</button>
+               </div>
+            </div>
+        </div>
+            `;
         }else{
             console.log('damn');
         }
